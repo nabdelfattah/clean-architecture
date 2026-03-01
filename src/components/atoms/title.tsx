6eb4ffaf592adc3db65e3edf40836511a4cfb00e
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-03-01 04:32:12 +0200

    feat(components): initialize Atomic Design pattern (Title atom only)

--- src/components/atoms/title.tsx
+++ src/components/atoms/title.tsx
@@ -1,3 +1,55 @@
-export default function Title() {
-  return <div></div>;
+import { cn } from "@/src/lib/cn";
+
+interface TitleProp {
+  type?: "h1" | "h2" | "h3" | "p";
+  color?: "black" | "white";
+  isCentered?: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function Title({
+  type = "h2",
+  color = "black",
+  isCentered = true,
+  className = "",
+  children,
+}: TitleProp) {
+  let content = (
+    <h2
+      className={cn(
+        "ds-leading-full ds-font-playfair font-normal ds-txt-lg",
+        color === "white" ? "ds-txt-white" : "ds-txt-black",
+        isCentered ? "text-center" : "",
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
+
+  if (type === "h1")
+    content = (
+      <h1
+        className={cn(
+          "ds-txt-black text-center ds-font-playfair font-normal ds-txt-xl ds-leading-full",
+          className,
+        )}
+      >
+        {children}
+      </h1>
+    );
+  else if (type === "p")
+    content = (
+      <p
+        className={cn(
+          "ds-txt-black ds-font-playfair ds-leading-full text-xl font-normal",
+          className,
+        )}
+      >
+        {children}
+      </p>
+    );
+
+  return content;
 }
