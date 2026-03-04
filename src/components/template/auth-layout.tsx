904afc740c09f744687d25e3a6a85d3f03354f40
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-03-04 18:55:35 +0200

    feat(layout): update auth-layout

--- src/components/template/auth-layout.tsx
+++ src/components/template/auth-layout.tsx
@@ -1,3 +1,17 @@
-export default function AuthLayout() {
-  return <></>;
+import { AuthHeader, Footer } from "@/src/components";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AuthHeader />
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
 }
